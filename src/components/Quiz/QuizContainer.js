78e3ce74bc2bb538894baa48f71047558cd0d095
Nikola Nesovic <doublen987@gmail.com>
2022-06-11 21:18:46 +0200

    Quizes now redirect to /quiz when invalid quizid

--- src/components/Quiz/QuizContainer.js
+++ src/components/Quiz/QuizContainer.js
@@ -1,6 +1,7 @@
 import React, { Component, useContext, useState } from "react";
+import { Redirect } from "react-router-dom";
 import { Switch, Route, useRouteMatch } from "react-router";
-import { AppContext, ChangeAppStateContext } from '../App'
+import { AppStateContext, ChangeAppStateContext } from '../App'
 import Quiz from "./Quiz/Quiz";
 import QuizEditor from "./QuizEditor/QuizEditor";
 import QuizCSS from './QuizContainer.module.css'
@@ -10,16 +11,26 @@ import Aux from "../hoc/Aux";
 function QuizContainer() {
 
         let {path, url} = useRouteMatch();
+        const appContext = useContext(AppStateContext);
 
         return (<div className={QuizCSS.quizpage}>
             
             <Switch>
-                <Route path={`${path}/:quizid`} render={(props) => (
-                    <Aux>
-                        <QuizList key={"quizlist-" + props.match.params.quizid} quizid={props.match.params.quizid}></QuizList>
-                        <Quiz key={"quiz-" + props.match.params.quizid} quizid={props.match.params.quizid}></Quiz>
-                    </Aux>
-                )}/>
+                <Route path={`${path}/:quizid`} render={(props) => {
+                    console.log(props.match.params.quizid)
+                  
+                    let redirect = <Redirect to={`/quiz`}></Redirect>;
+                    
+                    if(appContext.quizes.find(quiz => quiz.id == props.match.params.quizid)) {
+                        redirect = <Aux>
+                            <QuizList key={"quizlist-" + props.match.params.quizid} quizid={props.match.params.quizid}></QuizList>
+                            <Quiz key={"quiz-" + props.match.params.quizid} quizid={props.match.params.quizid}></Quiz>
+                        </Aux>
+                    }
+                    
+                    return(
+                    {...redirect}
+                )}}/>
                 <Route path={"/quiz"}>
                     <Aux>
                         <QuizList></QuizList>
